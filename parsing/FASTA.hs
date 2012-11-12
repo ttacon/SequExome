b7672ba0ef8a34ee5cc7f704911742f2aeb2327d
ttacon <ttacon@gmail.com>
2012-11-12 11:41:36 -0500

    adding changes to FASTA.hs

--- parsing/FASTA.hs
+++ parsing/FASTA.hs
@@ -1,14 +1,71 @@
 module FASTA (
-	Sequence
-	SequenceInfo
-	SequenceString
+	
+	parseGene
 
 	) where
 
+	{-
+	GeneSequence
+	SequenceInfo
+	SequenceString
+	-}
+
+import Text.ParserCombinators.Parsec
+import Text.Parsec.Pos (newPos)
 import Data.List
 import qualified System.Time
+import FastaSequenceTestData
+import Converter
 
 
 
-parseFastaFile :: String -> [Sequence]
+
+type FastaParser a = GenParser Char () a
+
+
+
+
+parseFastaFile :: String -> [Gene]
 parseFastaFile = parseFastaFile
+
+--testSeq :: [Char] -> Either ParseError [Gene]
+testSeq a = parse parseGene "(unknown)" a
+
+
+parseGene ::  FastaParser [Gene]
+parseGene = try (do many newline
+                    gI <- parseGeneInfo
+                    gL <- many1 parseGeneLine
+                    -- merge gL's into one?
+                    moreGenes <- parseGene
+                    return $ [Gene {geneInfo=gI,nucSequence=(concat gL)}]++moreGenes
+            )
+            <|> do  many newline
+                    gI <- parseGeneInfo
+                    gL <- many1 parseGeneLine
+                    --newline
+                    return $ [Gene {geneInfo=gI,nucSequence=(concat gL)}]
+
+
+parseGeneInfo :: FastaParser String
+parseGeneInfo = do  char '>'
+                    i <- many1 (noneOf "\n")
+                    newline
+                    return i
+
+parseGeneLine = do  i <- many1 (noneOf "\n>")
+                    newline
+                    return i
+
+
+
+
+
+
+
+
+
+
+
+
+
